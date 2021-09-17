7e4d68765a36f020391c8fc82fd5ee10f245f370
Leandroyyy <leandro.cavallari123@gmail.com>
2021-09-17 14:49:36 -0300

    começando a sorveteria

--- exercicio5-2/src/App.js
+++ exercicio5-2/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from './components/cabecalho/Cabecalho';
+import Corpo from './components/corpo/Corpo';
+import Rodape from './components/rodape/Rodape';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Cabecalho/>
+      <Corpo/>
+      <Rodape/>
+    </>
   );
 }
 
