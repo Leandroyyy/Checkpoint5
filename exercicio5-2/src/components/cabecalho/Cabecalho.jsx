b5bd1e354be125f39e6704473a9ac30d1b310600
Leandroyyy <leandro.cavallari123@gmail.com>
2021-09-23 21:25:14 -0300

    começando o segundo exercicio

---
+++ exercicio5-2/src/components/cabecalho/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import {Alinhar, Titulo} from "../../styled";
+
+
+export default function Cabecalho(){
+    return(
+        <Alinhar>
+            <Titulo>
+                <h1>Sorveteria São Joaquim</h1>
+                <p>A Melhor do Brasil</p>
+            </Titulo>
+        </Alinhar>
+    )
+}
---
+++ exercicio5-2/src/components/corpo/Corpo.jsx
@@ -0,0 +1,30 @@
+import { Borda, Sabores } from "../../styled";
+
+
+export default function Corpo(){
+    return(
+        <>
+            <Sabores>
+                <Borda>
+                    <h2>Sabores</h2>
+                    <ul>
+                        <li>teste</li>
+                        <li>teste</li>
+                        <li>teste</li>
+                    
+                    </ul>
+                
+                <h2>Acompanhamentos</h2>
+                <ul>
+                    <li>teste</li>
+                    <li>teste</li>
+                    <li>teste</li>
+                    <li>teste</li>
+                    <li>teste</li>
+                    
+                </ul>
+                </Borda>
+            </Sabores>
+        </>
+    )
+}
