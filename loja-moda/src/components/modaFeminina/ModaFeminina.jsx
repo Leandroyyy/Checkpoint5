708b364b545b44fd782a3821d10e212512aa90b6
Leandroyyy <leandro.cavallari123@gmail.com>
2021-09-24 15:35:59 -0300

    mandando tudo pro github

---
+++ loja-moda/src/components/modaFeminina/ModaFeminina.jsx
@@ -0,0 +1,9 @@
+
+export default function ModaFeminina(){
+
+    return(
+        <>
+            <h1>Hello mulher</h1>
+        </>
+    )
+}
---
+++ loja-moda/src/components/rodape/Rodape.jsx
@@ -0,0 +1,10 @@
+
+export default function Rodape(){
+    return(
+        <>
+            <footer>
+                <p>testando aqui o pezinho</p>
+            </footer>
+        </>
+    )
+}
