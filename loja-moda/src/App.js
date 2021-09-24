72dd522e838c755eee91260a69e838dd4f78220b
Leandroyyy <leandro.cavallari123@gmail.com>
2021-09-24 12:05:24 -0300

    feito as rotas e o inicio do styled components

--- loja-moda/src/App.js
+++ loja-moda/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import Menu from './components/menu/Menu';
+import "./App.css";
+import { Route , Switch} from "react-router-dom";
+import Menu from "./components/menu/Menu";
+import Home from "./components/home/Home";
+import ModaMasculina from "./components/modaMasculina/ModaMasculina";
+import ModaFeminina from "./components/modaFeminina/ModaFeminina";
+import ModaInfantil from "./components/ModaInfantil/ModaInfantil";
+import Sobre from "./components/sobre/Sobre";
+import Rodape from "./components/rodape/Rodape";
 
 function App() {
   return (
     <>
       <Menu/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/ModaMasculina" component={ModaMasculina}/>
+        <Route path="/ModaFeminina" component={ModaFeminina}/>
+        <Route path="/ModaInfantil" component={ModaInfantil}/>
+        <Route path="/Sobre" component={Sobre}/>
+      </Switch>
+      <Rodape/>
     </>
   );
 }
